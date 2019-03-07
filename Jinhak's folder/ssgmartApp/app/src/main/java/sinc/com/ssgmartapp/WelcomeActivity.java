641986305594a41da0d049231d392a925fbec891
weejinhak <wlsgkr91@gmail.com>
2019-03-07 15:04:19 +0900

    Add image file And Welcome Image file

--- Jinhak's folder/ssgmartApp/app/src/main/java/sinc/com/ssgmartapp/WelcomeActivity.java
+++ Jinhak's folder/ssgmartApp/app/src/main/java/sinc/com/ssgmartapp/WelcomeActivity.java
@@ -121,7 +121,8 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
+        //주석 풀면 설치하고 한번만 실행됨.
+        //prefManager.setFirstTimeLaunch(false);
         startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
         finish();
     }
